efdf10f27aaf1eb786a21b5e12fb6f3786078f67
Harry Parkes <harrydparkes@proton.me>
2025-02-18 14:26:51 +1300

    Chore: Python code is now more Ruff compliant

--- python/youtube-music-album-art-downloader-2.py
+++ python/youtube-music-album-art-downloader-2.py
@@ -1,22 +1,33 @@
-import aiohttp
 import asyncio
-import sys
 import os
+import pathlib
+import sys
 
-from bs4 import BeautifulSoup
+import aiohttp
 import piexif
+from bs4 import BeautifulSoup, NavigableString
 
 
-def print_error(*args, **kwargs) -> None:
+def print_error(*args: object, **kwargs) -> None:
+    """Print to stderr instead of stdout.
+
+    :param args: Values to be printed.
+    :param kwargs: Key word arguments to pass to ``print``.
+    """
     print(*args, file=sys.stderr, **kwargs)
 
 
 async def download_album_art(
-    session: aiohttp.ClientSession, link: str, album_title: str, youtube_album_code: str
+    session: aiohttp.ClientSession,
+    link: str,
+    album_title: str,
+    youtube_album_code: str,
 ) -> None:
     """Asyncronously download the album art image from the specified link.
 
-    :param session: The HTTP session to send the request with (reuses connection to improve speed).
+    :param session: The HTTP session to send the request with
+        (reuses connection to improve speed).
+
     :param link: The URL to the image to download.
     :param album_title: The title of the album whose art to download.
     """
@@ -30,7 +41,7 @@ async def download_album_art(
                 "GPS": {},
                 "1st": {},
                 "thumbnail": None,
-            }
+            },
         )
         piexif.insert(exif_data, await album_art_response.read(), filename)
 
@@ -43,27 +54,39 @@ async def request_album_page(
 ) -> None:
     """Asyncronously fetch the YouTube Music album page at the specified URL.
 
-    :param session: The HTTP session to send the request with (reuses connection to improve speed).
-    :param album_art_session: The HTTP session to send album art image request with (reuses connection to improve speed).
+    :param session: The HTTP session to send the request with
+        (reuses connection to improve speed).
+
+    :param album_art_session: The HTTP session to send album art image request
+        with (reuses connection to improve speed).
+
     :param link: The URL to the YouTube Music album page.
     :param download_tasks: A list of tasks to append the image download task to.
     """
     async with session.get(link) as album_page_response:
         parsed_document = BeautifulSoup(
-            await album_page_response.text(), features="html.parser"
+            await album_page_response.text(),
+            features="html.parser",
         )
 
         album_title_element = parsed_document.find("meta", attrs={"name": "title"})
-        if album_title_element is None:
+        if album_title_element is None or isinstance(
+            album_title_element,
+            NavigableString,
+        ):
             print_error("No album title for", link)
             return
 
         album_title = album_title_element.attrs["content"]
 
         album_art_link_element = parsed_document.find(
-            "meta", attrs={"property": "og:image"}
+            "meta",
+            attrs={"property": "og:image"},
         )
-        if album_art_link_element is None:
+        if album_art_link_element is None or isinstance(
+            album_art_link_element,
+            NavigableString,
+        ):
             print_error("No album art link for", link)
             return
 
@@ -74,14 +97,40 @@ async def request_album_page(
         download_tasks.append(
             asyncio.create_task(
                 download_album_art(
-                    album_art_session, album_art_link, album_title, link[-17:]
-                )
-            )
+                    album_art_session,
+                    album_art_link,
+                    album_title,
+                    link[-17:],
+                ),
+            ),
         )
 
 
-async def main():
-    # Read in a file consisting of comma seperated URLs
+async def run_downloads(links_to_download: list[str]) -> None:
+    """Download album art images for each album link in ``links_to_download``.
+
+    :param links_to_download: A list of links to YouTube Music album pages.
+    """
+    download_tasks = []
+    async with (
+        aiohttp.ClientSession() as youtube_music_session,
+        aiohttp.ClientSession() as google_user_content_session,
+    ):
+        tasks = [
+            request_album_page(
+                youtube_music_session,
+                google_user_content_session,
+                link,
+                download_tasks,
+            )
+            for link in links_to_download
+        ]
+        await asyncio.gather(*tasks)
+        await asyncio.gather(*download_tasks)
+
+
+def main() -> None:
+    """Figure out which imgages already exist and download new ones."""
     with open("links.txt") as link_file:
         links = link_file.read().split(", ")
 
@@ -93,20 +142,12 @@ async def main():
     links_to_download = [
         link for link in links if link[-17:].encode() not in existing_images
     ]
-    # Ensure folder to save images in is present
-    os.makedirs("album_arts", exist_ok=True)
 
-    download_tasks = []
-    async with aiohttp.ClientSession() as youtube_music_session, aiohttp.ClientSession() as google_user_content_session:
-        tasks = [
-            request_album_page(
-                youtube_music_session, google_user_content_session, link, download_tasks
-            )
-            for link in links_to_download
-        ]
-        await asyncio.gather(*tasks)
-        await asyncio.gather(*download_tasks)
+    # Ensure folder to save images in is present
+    pathlib.Path("album_arts").mkdir(exist_ok=True)
+
+    asyncio.run(run_downloads(links_to_download))
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
